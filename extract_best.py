0165bbd15f4a5fbcd350f4cd5fbe8d8d96253123
Micah Bowles <micah.bowles@postgrad.manchester.ac.uk>
2021-08-23 14:45:37 +0100

    Reduced data bug fix

--- extract_best.py
+++ extract_best.py
@@ -2,6 +2,8 @@ import os
 import sys
 import shutil
 import pandas as pd
+import utils
+import configparser as ConfigParser
 
 # Read in config file
 args        = utils.parse_args()
@@ -14,6 +16,14 @@ early_stopping = config.getboolean('training', 'early_stopping')
 
 # -----------------------------------------------------------------------------
 # Extracting Path of Best model
+# Load training csv
+path_supliment = config['data']['augment']
+if path_supliment in ['True', 'False']:
+    path_supliment=''
+else:
+    path_supliment+='/'
+csv_path = config['output']['directory'] +'/'+ path_supliment + config['output']['training_evaluation']
+df = pd.read_csv(csv_path)
 best = df.iloc[list(df['validation_update'])].iloc[-1]
 best_epoch = int(best.name)
 path_supliment = config['data']['augment']
@@ -27,8 +37,8 @@ OUT_PATH = MODEL_EVAL_PATH[:7] + 'reduced/' + MODEL_EVAL_PATH[7:] #-file name
 files = os.listdir(MODEL_EVAL_PATH)
 
 os.makedirs(OUT_PATH, exist_ok=True)
-tmp = shutil.copy(OUT_PATH+str(best_epoch)+'.pt'
+tmp = shutil.copy(MODEL_PATH, OUT_PATH)
 for f in files:
     if '.pt' not in f:
-        if (len(f.split('_'))!=4) or ('amap' in f):
-            tmp = shutil.copy(MODEL_EVAL_PATH+f, OUT_PATH)
+        #if (len(f.split('_'))!=4) or ('amap' in f): # Uncomment to ignore MP4 images if I like
+        tmp = shutil.copy(MODEL_EVAL_PATH+f, OUT_PATH)
