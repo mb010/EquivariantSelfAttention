Softmax bug fix.

networks/SelfAttention_Bowles2021.py
@@ -25,6 +25,7 @@ class AGRadGalNet(nn.Module):
         normalisation = attention_normalisation
         kernel_size = int(kernel_size)
         imsize = int(imsize)
+        n_classes = int(n_classes)
         AG = int(attention_gates)
         assert aggregation_mode in ['concat', 'mean', 'deep_sup', 'ft'], 'Aggregation mode not recognised. Valid inputs include concat, mean, deep_sup or ft.'
         assert normalisation in ['sigmoid','range_norm','std_mean_norm','tanh','softmax'], f'Nomralisation not implemented. Can be any of: sigmoid, range_norm, std_mean_norm, tanh, softmax'
@@ -239,17 +240,15 @@ class AGRadGalNet(nn.Module):
         if type(out)==list:
             if self.aggregation_mode == 'mean':
                 out = torch.mean(torch.stack(out),dim=[0]) #This will output a single vector for classification instead of a list of classifications.
-                out = F.softmax(out,dim=1)
             elif self.aggregation_mode == 'deep_sup':
                 out = torch.mean(torch.stack(out),dim=[0])
-                out = F.softmax(out,dim=1)
             elif self.aggregation_mode == 'ft': # Might not be a problem
-                out = F.softmax(out,dim=1)
+                out = out
             else:
                 raise NotImplementedError
             return out
         else:
-            return F.softmax(out,dim=1)
+            return out
 
 
 

networks/e2Nets.py
@@ -221,7 +221,7 @@ class DNSteerableLeNet(nn.Module):
         self,
         base='DNSteerableLeNet',
         in_chan=1,
-        out_chan=2,
+        n_classes=2,
         imsize=150,
         kernel_size=5,
         N=8,
@@ -229,6 +229,7 @@ class DNSteerableLeNet(nn.Module):
         number_rotations=None):
         super(DNSteerableLeNet, self).__init__()
         kernel_size = int(kernel_size)
+        out_chan = int(n_classes)
 
         if number_rotations != None:
             N = int(number_rotations)

networks/e2attention.py
@@ -32,6 +32,7 @@ class DNSteerableAGRadGalNet(nn.Module):
         N = int(number_rotations)
         kernel_size = int(kernel_size)
         imsize = int(imsize)
+        n_classes = int(n_classes)
         assert aggregation_mode in ['concat', 'mean', 'deep_sup', 'ft'], 'Aggregation mode not recognised. Valid inputs include concat, mean, deep_sup or ft.'
         assert normalisation in ['sigmoid','range_norm','std_mean_norm','tanh','softmax'], f'Nomralisation not implemented. Can be any of: sigmoid, range_norm, std_mean_norm, tanh, softmax'
         assert AG in [0,1,2,3], f'Number of Attention Gates applied (AG) must be an integer in range [0,3]. Currently AG={AG}'
@@ -288,17 +289,15 @@ class DNSteerableAGRadGalNet(nn.Module):
         if type(out)==list:
             if self.aggregation_mode == 'mean':
                 out = torch.mean(torch.stack(out),dim=[0]) #This will output a single vector for classification instead of a list of classifications.
-                out = F.softmax(out,dim=1)
             elif self.aggregation_mode == 'deep_sup':
                 out = torch.mean(torch.stack(out),dim=[0])
-                out = F.softmax(out,dim=1)
             elif self.aggregation_mode == 'ft': # Might not be a problem
-                out = F.softmax(out,dim=1)
+                out = out
             else:
                 raise NotImplementedError
             return out
         else:
-            return F.softmax(out,dim=1)
+            return out
 
 
 